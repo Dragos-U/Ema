3a0c057238d0b022aea18f0aed45efd16576ac9d
Dragos Ursu <dragos.ursu15@gmail.com>
2023-10-19 18:06:44 +0300

    project created

---
+++ src/main/java/ro/itschool/ema/EmaApplication.java
@@ -0,0 +1,13 @@
+package ro.itschool.ema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/itschool/ema/EmaApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.itschool.ema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
