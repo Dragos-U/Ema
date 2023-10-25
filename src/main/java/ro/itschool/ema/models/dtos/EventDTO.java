e024ddb84a113d16007ce13ceaf6bad2fc08bb64
TatianaMamaliga <125645241+TatianaMamaliga@users.noreply.github.com>
2023-10-25 16:56:56 +0300

    added validation to DTO files

--- src/main/java/ro/itschool/ema/models/dtos/EventDTO.java
+++ src/main/java/ro/itschool/ema/models/dtos/EventDTO.java
@@ -1,5 +1,9 @@
 package ro.itschool.ema.models.dtos;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,9 +12,17 @@ import java.util.Date;
 @Data
 public class EventDTO implements Serializable {
     private Long id;
+    @NotNull(message = "Field cannot be null.")
     private String eventName;
+    @NotNull(message = "Field cannot be null.")
+    @Past(message = "Invalid date.")
     private Date eventDate;
+    @NotNull(message = "Field cannot be null.")
     private String eventVenue;
+    @NotNull(message = "Field cannot be null.")
     private String eventDescription;
+    @NotNull(message = "Field cannot be null.")
+    @Min(value = 10, message = "Participant number should be greater than 10.")
+    @Max(value = 10000, message = "Participant number should be less than 10000.")
     private int maxNumOfParticipants;
 }
--- src/main/java/ro/itschool/ema/models/dtos/OrganizerDTO.java
+++ src/main/java/ro/itschool/ema/models/dtos/OrganizerDTO.java
@@ -1,5 +1,8 @@
 package ro.itschool.ema.models.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,9 +11,16 @@ import java.util.Date;
 @Data
 public class OrganizerDTO implements Serializable {
     private Long id;
+    @NotNull(message = "Field cannot be null.")
     private String firstName;
+    @NotNull(message = "Field cannot be null.")
     private String lastName;
+    @NotNull(message = "Field cannot be null.")
+    @Past(message = "Invalid date.")
     private Date dateOfBirth;
+    @NotNull(message = "Field cannot be null.")
+    @Email(message = "Invalid email. Please enter a valid email.")
     private String email;
+    @NotNull(message = "Field cannot be null.")
     private String phoneNumber;
 }
